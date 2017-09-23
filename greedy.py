9eeffba70082478027f45c7eeb804298d3788211
Chandonlol <chendilin92@gmail.com>
2017-09-23 13:53:32 -0500

    greedy finish

---
+++ greedy.py
@@ -0,0 +1,82 @@
+import numpy as np
+import mpmath as math
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from collections import deque
+import csv
+
+
+
+# direction definition:
+# 0: left 1: right 2: up 3: down
+
+def heuristic(x,y):
+    return((x[0] - y[0])**2 + (x[1]-y[1])**2)
+
+
+
+def Greedy():
+    import read_maze
+    maze,visited,unavaiable,start,goal,rows,columns = read_maze.generate_maze()
+    print(start,goal)
+    prev = [[[-1,-1] for x in range(columns)] for y in range(rows)]  # record all the history
+    path = []
+    queue = [start]
+    queue = deque(queue)
+    frontier = []
+    steps = 0
+    while(len(queue)!=0):
+        node_now = queue.popleft()
+        x = node_now[0]
+        y = node_now[1]
+        unavaiable[x][y] = 1
+        if (x == goal[0] and y == goal[1]):
+            print("solution found")
+            pos_now = goal
+            while (pos_now!=start):
+                path.append(pos_now)
+
+                pos_now = prev[pos_now[0]][pos_now[1]]
+                maze[pos_now[0]][pos_now[1]] = '.'
+            maze[start[0]][start[1]] = "P"
+            path.reverse()
+            print(path)
+            length = len(path)
+            print("Total length of the path is " + str(length))
+            print("Number of expanded nodes is " + str(steps))
+            # write it
+            with open('test_file.csv', 'w') as csvfile:
+                writer = csv.writer(csvfile)
+                for r in maze:
+                    r = ''.join(str(word) for word in r)
+                    print(r)
+                    writer.writerow(r)
+            return
+        # loop over all possible nodes
+        if (unavaiable[x+1][y] == 0 and visited[x+1][y] == 0): # right
+            heu = heuristic([x+1,y],goal)
+            frontier.append([x+1,y,heu])
+            prev[x+1][y] = [x, y]
+        if (unavaiable[x-1][y] == 0 and visited[x-1][y] == 0): # left
+            heu = heuristic([x-1,y],goal)
+            frontier.append([x-1,y,heu])
+            prev[x-1][y] = [x, y]
+        if (unavaiable[x][y-1] == 0 and visited[x][y-1] == 0): # down
+            heu = heuristic([x,y-1],goal)
+            frontier.append([x,y-1,heu])
+            prev[x][y-1] = [x, y]
+        if (unavaiable[x][y+1] == 0 and visited[x][y+1] == 0): # up
+            heu = heuristic([x,y+1],goal)
+            frontier.append([x,y+1,heu])
+            prev[x][y+1] = [x, y]
+
+        expand = min(frontier, key = lambda t: t[2])
+        queue.append(expand)
+        frontier.remove(expand)
+        steps += 1
+
+def main():
+    Greedy()
+
+if __name__ == "__main__":
+    main()
--- read_maze.py
+++ read_maze.py
@@ -9,26 +9,22 @@ def mazeinput (maze,filename):
     columns = mazeFile.readlines()
     for column in columns:
         column = column.strip()
-        print(column)
         row = [i for i in column]
         maze.append(row)
 
-maze =[]
-mazeinput(maze,"mediumMaze.txt")
+def generate_maze():
+    maze =[]
+    mazeinput(maze,"openMaze.txt")
+    [rows,columns] = np.shape(maze)
+    visited = [[0 for x in range(columns)] for y in range(rows)]  # visited = 1 means the place has been visited
+    unavaiable = [[0 for x in range(columns)] for y in range(rows)] # unavaible = 1 means there is a wall in that position
 
-[rows,columns] = np.shape(maze)
-visited = [[0 for x in range(columns)] for y in range(rows)]  # visited = 1 means the place has been visited
-unavaiable = [[0 for x in range(columns)] for y in range(rows)] # unavaible = 1 means there is a wall in that position
-
-for i in range(rows):
-    for j in range(columns):
-        if maze[i][j] == '%':
-            unavaiable[i][j] = 1
-        if maze[i][j] == 'P':
-            visited[i][j] = 1
-            start = [i,j]
-        if maze[i][j] == '.':
-            goal = [i,j]
-
-print(visited)
-print(start)
+    for i in range(rows):
+        for j in range(columns):
+            if maze[i][j] == '%':
+                unavaiable[i][j] = 1
+            if maze[i][j] == 'P':
+                start = [i,j]
+            if maze[i][j] == '.':
+                goal = [i,j]
+    return maze,visited,unavaiable,start,goal,rows,columns
